39fe5dbe914929f7ee66c99bd7787fdc21fa886d
AndreSimao <andresimaodesouza@gmail.com>
2026-06-14 23:19:33 -0300

    fix: correct TX battery status variable and increase transmission interval

--- lib/radio/radio.cpp
+++ lib/radio/radio.cpp
@@ -2,11 +2,11 @@
 
 SX1262 radio = new Module(LORA_NSS, LORA_DIO1, LORA_NRST, LORA_BUSY);
 unsigned long last_tx_time = 0;
-const unsigned long tx_interval = 1e3;
+const unsigned long tx_interval = 1e4;
 TelemetryData data = {0};
 
 bool enable_radio(void) {
-    // Carrier Frequency: 915MHz
+     // Carrier Frequency: 915MHz
     // Bandwidth: 125.0kHz
     // Spreading Factor: 7
     // Coding Rate: 5
@@ -34,18 +34,18 @@ bool send_data(TelemetryData data) {
     Serial.print(F("Water Level: ")); Serial.print(data.water_lvl); Serial.println(F(" m"));
     Serial.print(F("Turbidity:   ")); Serial.print(data.turbidity); Serial.println(F(" NTU"));
     Serial.print(F("Water Pump:  ")); Serial.println(data.pump_status ? "ON" : "OFF");
-    Serial.print(F("TX Battery:  ")); Serial.print(data.bat_status); Serial.println(F(" %"));
+    Serial.print(F("TX Battery:  ")); Serial.print(data.bat_percent); Serial.println(F(" %"));
 
     digitalWrite(LED_GREEN, LOW);
     int transmitState = radio.transmit((uint8_t*)&data, sizeof(TelemetryData));
     digitalWrite(LED_GREEN, HIGH);
 
     if (transmitState == RADIOLIB_ERR_NONE) {
-      Serial.println(F("Success!"));
-      return true;
+        Serial.println(F("Success!"));
+        return true;
     } else {
-      Serial.print(F("Failed! Error code: "));
-      Serial.println(transmitState);
-      return false;
+        Serial.print(F("Failed! Error code: "));
+        Serial.println(transmitState);
+        return false;
     }
 }
